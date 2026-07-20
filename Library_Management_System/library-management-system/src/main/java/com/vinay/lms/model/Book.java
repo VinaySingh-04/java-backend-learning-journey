4502e0323b0f6048882f771a26781f3659923930
VinaySingh-04 <vinaygauraniya12@gmail.com>
2026-07-20 14:37:28 +0530

    Add getters and setters to Book entity

--- Library_Management_System/library-management-system/src/main/java/com/vinay/lms/model/Book.java
+++ Library_Management_System/library-management-system/src/main/java/com/vinay/lms/model/Book.java
@@ -17,6 +17,55 @@ public class Book {
         this.category=category;
         this.price=price;
         this.quantity=quantity;
+    }
 
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if(price > 0){
+            this.price = price;
+        }
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
