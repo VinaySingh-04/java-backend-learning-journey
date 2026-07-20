1ec1f7e162f24537f2605c4ee2e006cd09583c12
VinaySingh-04 <vinaygauraniya12@gmail.com>
2026-07-20 14:04:11 +0530

    Add constructors to Book entity

--- Library_Management_System/library-management-system/src/main/java/com/vinay/lms/model/Book.java
+++ Library_Management_System/library-management-system/src/main/java/com/vinay/lms/model/Book.java
@@ -1,4 +1,22 @@
 package com.vinay.lms.model;
 
 public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private String category;
+    private double price ;
+    private int quantity;
+
+    public Book(){}
+
+    public Book(int bookId,String title,String author,String category,double price,int quantity){
+        this.bookId=bookId;
+        this.title=title;
+        this.author=author;
+        this.category=category;
+        this.price=price;
+        this.quantity=quantity;
+
+    }
 }
